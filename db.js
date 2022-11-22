db4ba9e7f4bdb63d2294fba22e08127edf515fe0
Léna Abel <lena31.abel@gmail.com>
2022-11-22 17:01:37 +0100

    Add gitignore

---
+++ db.js
@@ -0,0 +1,12 @@
+const Pool = require("pg").Pool
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "tp_note1",
+    password: "0310",
+    port: 5432
+
+});
+
+module.exports = pool

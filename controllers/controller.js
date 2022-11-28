4fde3bae1e4de561cb2fde1e3a56f788d63db48f
Léna Abel <lena31.abel@gmail.com>
2022-11-29 00:35:20 +0100

    Add controllers to project

--- controllers/controller.js
+++ controllers/controller.js
@@ -14,12 +14,30 @@ exports.getAllLaureatesF1 = (req, res) => {
     });
 };
 
+exports.getLaureatesByIdF2 = (req, res) => {
+    const idLaureate = parseInt(req.params.idLaureate);
+    console.log("ID = " + req.params.idLaureate);
+    services.getLaureatesByIdF2([idLaureate], (error, results) => {
+        console.log(chalk.green("Request for F2 is a success!"));
+        if (error) {
+            console.log(error);
+            res.status(400).send({success: 0, data: error});
+        }
+        res.status(200).json(results);
+    });
+}
 
 
-
-
-
-
+exports.moreThanOnePrizeF3 = (req, res) => {
+    services.moreThanOnePrizeF3((error, results) => {
+        console.log(chalk.green("Request for F3 is a success!"));
+        if (error) {
+            console.log(error);
+            res.status(400).send({success: 0, data: error});
+        }
+        res.status(200).json(results);
+    });
+};
 
 
 exports.getCategoriesF4 = (req, res) => {
@@ -30,60 +48,10 @@ exports.getCategoriesF4 = (req, res) => {
         }
         return res.status(200).send({success: 1, data: results});
     });
-    /*res.render("list_categories/list_categories.hbs", ({
-        categories: list_categories
-    }));*/
 }
 
-exports.getLaureatesByIdF2 = (req, res) => {
-    console.log(chalk.green("Request for F2 is a success!"));
-    let idLaureate = req.params.idLaureate;
-    idLaureate = typeof idLaureate === 'undefined' ? "" : idLaureate;
-    // DEBUG
-    //console.log(req.params.idLaureate);
-    if (validator.isEmpty(idLaureate) || idLaureate === '{idLaureate}' || idLaureate === 'undefined') {
-        services.getAllLaureatesF1((error,results)=>{
-            if(error){
-                console.log(error);
-                return res.status(400).send({success:0,data:error});
-            }
-            return res.status(200).send({success:1,data:results});
-        });
-    } else if (validator.isInt(idLaureate)) {
-        services.getLaureatesByIdF2(idLaureate, (error, results) => {
-            if (error) {
-                console.log(error);
-                return res.status(400).send({success: 0, data: error});
-            }
-            return res.status(200).send({success: 1, data: results});
-        })
-    } else {
-        return res.status(400).send({success: 0, data: "Bad request"});
-    }
-};
-
-exports.numberPrizesF3 = (req, res) => {
-    services.numberPrizesF3((error, results) => {
-        console.log(chalk.green("Request for F3 is a success!"));
-        if (error) {
-            return res.status(400).send({success: 0, data: error});
-        }
-        return res.status(200).send({success: 1, data: results});
-    });
-};
-
-exports.numberLaureatesF4 = (req, res) => {
-    services.numberLaureatesF4((error, results) => {
-        console.log(chalk.green("Request for F4 is a success!"));
-        if (error) {
-            return res.status(400).send({success: 0, data: error});
-        }
-        return res.status(200).send({success: 1, data: results});
-    });
-};
-
-exports.moreThanOnePrizeF5 = (req, res) => {
-    services.moreThanOnePrizeF5((error, results) => {
+exports.mostPrizePerCategoryF5 = (req, res) => {
+    services.mostPrizePerCategoryF5((error, results) => {
         console.log(chalk.green("Request for F5 is a success!"));
         if (error) {
             return res.status(400).send({success: 0, data: error});
@@ -93,9 +61,9 @@ exports.moreThanOnePrizeF5 = (req, res) => {
 };
 
 
-exports.mostPrizePerCategoryF7 = (req, res) => {
-    services.mostPrizePerCategoryF7((error, results) => {
-        console.log(chalk.green("Request for F7 is a success!"));
+exports.getNumLaureatesPerYearF6 = (req, res) => {
+    services.getNumLaureatesPerYearF6((error, results) => {
+        console.log(chalk.green("Request for F6 is a success!"));
         if (error) {
             return res.status(400).send({success: 0, data: error});
         }
@@ -103,8 +71,9 @@ exports.mostPrizePerCategoryF7 = (req, res) => {
     });
 };
 
-exports.getNumLaureatesPerYearF8 = (req, res) => {
-    services.getNumLaureatesPerYearF8((error, results) => {
+
+exports.getYearsWithoutPrizesF7 = (req, res) => {
+    services.getYearsWithoutPrizesF7((error, results) => {
         console.log(chalk.green("Request for F8 is a success!"));
         if (error) {
             return res.status(400).send({success: 0, data: error});
@@ -113,42 +82,28 @@ exports.getNumLaureatesPerYearF8 = (req, res) => {
     });
 };
 
-exports.getLaureatesPrizesF9 = (req, res) => {
-    console.log(chalk.green("Request for F9 is a success!"));
-    let idLaureate = req.params.idLaureate;
-    idLaureate = typeof idLaureate === 'undefined' ? "" : idLaureate;
-    // DEBUG
-    //console.log(req.params.idLaureate);
-    if (validator.isEmpty(idLaureate) || idLaureate === '{idLaureate}' || idLaureate === 'undefined') {
-        services.getAllLaureatesF1((error,results)=>{
-            if(error){
-                console.log(error);
-                return res.status(400).send({success:0,data:error});
-            }
-            return res.status(200).send({success:1,data:results});
-        });
-    } else if (validator.isInt(idLaureate)) {
-        services.getLaureatesPrizesF9(idLaureate, (error, results) => {
+
+exports.allYearsPrizesSortedF8 = (req, res) => {
+    let sort = req.query.sort;
+    sort = typeof sort === 'undefined' ? "" : sort;
+
+    console.log(req.query.sort);
+
+    if (validator.isEmpty(sort) || sort === '{sort}' || sort === 'undefined') {
+        return res.status(400).send({success: 0, data: error});
+    } else if ((sort === "asc_laureates" ) || (sort === "desc_laureates")) {
+        services.allYearsPrizesSortedF8(sort,(error, results) => {
+            console.log(chalk.green("Request for F8 is a success!"));
             if (error) {
-                console.log(error);
                 return res.status(400).send({success: 0, data: error});
             }
             return res.status(200).send({success: 1, data: results});
-        })
+        });
     } else {
-        return res.status(400).send({success: 0, data: "Bad request!"});
+        return res.status(400).send({success: 0, data: "Bad request! You need to specify +laureates or -laureates"});
     }
 };
 
-exports.getYearsWithoutPrizesF10 = (req, res) => {
-    services.getYearsWithoutPrizesF10((error, results) => {
-        console.log(chalk.green("Request for F10 is a success!"));
-        if (error) {
-            return res.status(400).send({success: 0, data: error});
-        }
-        return res.status(200).send({success: 1, data: results});
-    });
-};
 
 exports.allYearsPrizesSortedF11 = (req, res) => {
     console.log(chalk.green("Request for F11 is a success!"));
@@ -171,42 +126,6 @@ exports.allYearsPrizesSortedF11 = (req, res) => {
     }
 };
 
-exports.laureatesWhoMatchFilterF12 = (req, res) => {
-    console.log(chalk.green("Request for F12 is a success!"));
-    const firstname = req.query.firstname;
-    const surname = req.query.surname;
-    const category = req.query.category;
-
-    services.laureatesWhoMatchFilterF12(firstname, surname, category, (error, results) => {
-        console.log(chalk.green("Request for F12 is a success!"));
-        if (error) {
-            return res.status(400).send({success: 0, data: error});
-        }
-        return res.status(200).send({success: 1, data: results});
-    });
-};
-
-exports.filteredLaureatesF12 = (req, res) => {
-    filter = req.query.filter;
-    filter = typeof filter === 'undefined' ? "" : filter;
-    // DEBUG
-    console.log("req.query.filter=" + req.query.filter);
-    if (validator.isEmpty(filter) || filter === '{filter}' || filter === 'undefined') {
-        return res.status(400).send({success:0, data:"Bad request!"});
-    } else if (validator.isAscii(filter)) {
-        console.log(chalk.green("F12 - Request is a success!"));
-        services.filteredLaureatesF12(filter, (error, results) => {
-            if (error) {
-                console.log("F12 - " + error);
-                return res.status(400).send({success: 0, data: error});
-            }
-            console.log(chalk.green("F12 - Done with the processing of the request."));
-            return res.status(200).send({success: 1, data: results});
-        });
-    } else {
-        return res.status(400).send({success: 0, data: "F12 - Bad request!"});
-    }
-};
 
 exports.deleteLaureateF13 = (req, res) => {
     console.log(chalk.green("Request for F13 is a success!"));
@@ -273,92 +192,4 @@ exports.updateLaureateF14 = (req, res) => {
     } else {
         return res.status(400).send({success: 0, data: "Bad request"});
     }
-}
-
-exports.addLaureateF15 = (req, res) => {
-    console.log(chalk.green("Request for F15 is a success!"));
-    let year = req.query.year;
-    let category = req.query.category;
-    let firstname = req.query.firstname;
-    let surname = req.query.surname;
-    let motivation = req.query.motivation;
-
-    // DEBUG
-
-    console.log("Year: " +year);
-    console.log("Category: " + category);
-    console.log("firstname: " + firstname);
-    console.log("surname: " + surname);
-    console.log("Motiv: " + motivation);
-
-    year = typeof year === 'undefined' ? "" : year;
-    category = typeof category === 'undefined' ? "" : category;
-    firstname = typeof firstname === 'undefined' ? "" : firstname;
-    surname = typeof surname === 'undefined' ? "" : surname;
-    motivation = typeof motivation === 'undefined' ? "" : motivation;
-
-    if (validator.isEmpty(year) || year === '{year}' || year === 'undefined' ||
-        validator.isEmpty(category) || category === '{category}' || category === 'undefined' ||
-        validator.isEmpty(firstname) || firstname === '{firstname}' || firstname === 'undefined' ||
-        validator.isEmpty(surname) || surname === '{surname}' || surname === 'undefined' ||
-        validator.isEmpty(motivation) || motivation === '{motivation}' || motivation === 'undefined'){
-        return res.status(400).send({success:0,data: "Bad request. Please specify a year, category, firstname, surname and motivation to add a laureate."});
-    } else if (validator.isInt(year) && validator.isAscii(category) && validator.isAscii(firstname) && validator.isAscii(surname) && validator.isAscii(motivation)) {
-        services.addLaureateF15(year, category, firstname, surname, motivation, (error, results) => {
-            if (error) {
-                console.log(error);
-                return res.status(400).send({success: 0, data: error});
-            }
-            return res.status(200).send({success: 1, data: results});
-        })
-    } else {
-        return res.status(400).send({success: 0, data: "Bad request"});
-    }
-}
-
-exports.list_categories = (req, res) => {
-    console.log(chalk.green("Request for listing categories is a success!"));
-    console.log("Category: " + req.body.categories);
-
-    res.render("partials/list_categories.hbs", ({
-        categories: services.getCategoriesF6((error, results) => {
-            if (error) return error
-            else return results
-        }),
-        category: req.body.categories,
-        laureates: services.laureatesWhoMatchFilterF12(null, null, req.body.categories, (error, results) => {
-            if(error) return error;
-            else return results;
-        })
-    }));
-}
-
-exports.add_laureates = (req, res) => {
-    console.log(chalk.green("Request to add laureate is a success!"));
-    res.render("partials/add_laureate.hbs", ({
-        categories: services.getCategoriesF6((error, results) => {
-            if (error) return error
-            else return results
-        })
-    }))
-}
-
-exports.new_laureates = (req, res) => {
-    console.log(chalk.green("Request for adding a laureate after validation is a success!"));
-    let year = req.body.year;
-    let category = req.body.categories;
-    let firstname = req.body.firstname;
-    let surname = req.body.surname;
-    let motivation = req.body.motivation;
-    console.log("Year: " + year + " Category: " + category + " Firstname: " + firstname + " Surname: " + surname + " Motivation " + motivation);
-    res.render("partials/add_laureate.hbs", ({
-        categories: services.getCategoriesF6((error, results) => {
-            if (error) return error
-            else return results
-        }),
-        laureates: services.addLaureateF15(year, category, firstname, surname, motivation, (error, results) => {
-            if(error) return error;
-            else return results;
-        })
-    }));
 }
